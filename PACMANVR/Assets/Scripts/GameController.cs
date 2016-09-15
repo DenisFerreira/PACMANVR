59a3e8b75ba9df59bde4c8e89ff89b41f689bb46
Thiago Paiva <thiago.paiva@lsitec.org.br>
2016-09-15 17:51:49 -0300

    [Main] Set Random Path for Enemies

--- PACMANVR/Assets/Scripts/GameController.cs
+++ PACMANVR/Assets/Scripts/GameController.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
-	public GameObject maze;
+	public GameObject maze, player;
 	public GameObject[] monsters;
-	public GameObject player;
+
 	// Use this for initialization
 	void Start () {
-		GameObject maze1 = Instantiate(maze) as GameObject;
-		Instantiate (player, GameObject.FindGameObjectWithTag("SpawnPlayer").transform.position, Quaternion.identity);
+		Instantiate(maze);
+		Instantiate (player, GameObject.FindGameObjectWithTag ("SpawnPlayer").transform.position, Quaternion.identity);
 		ArrayList usedValues = new ArrayList ();
 			foreach (GameObject spawn in GameObject.FindGameObjectsWithTag("SpawnMonster")) {
 				int random = Random.Range (0, monsters.Length);
@@ -19,7 +19,7 @@ public class GameController : MonoBehaviour {
 				Instantiate (monsters [random], spawn.transform.position, Quaternion.identity);
 		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
